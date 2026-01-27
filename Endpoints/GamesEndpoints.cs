e33c17133882b6c60a2e0ecf7a83c91875f82610
Alfredo Aramburo <alfredo.aramburo420@gmail.com>
2026-01-26 18:03:50 -0600

    add: validacion a dtos

--- Endpoints/GamesEndpoints.cs
+++ Endpoints/GamesEndpoints.cs
@@ -19,7 +19,9 @@ public static class GamesEndpoints
     public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
     {
 
-        var group = app.MapGroup("/games");
+        var group = app.MapGroup("/games").WithParameterValidation();
+
+
 
         //  GET /games
         group.MapGet("/", () => games);
