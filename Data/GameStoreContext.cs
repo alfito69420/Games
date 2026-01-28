e86dc370b5a372c8fb4939901cded3ac173548ab
Alfredo Aramburo <alfredo.aramburo420@gmail.com>
2026-01-27 18:51:50 -0600

    add: base de datos, entidades y migraciones

---
+++ Data/GameStoreContext.cs
@@ -0,0 +1,10 @@
+namespace GameStore.Data;
+
+using Microsoft.EntityFrameworkCore;
+using GameStore.Entities;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDB();
 
 app.Run();
